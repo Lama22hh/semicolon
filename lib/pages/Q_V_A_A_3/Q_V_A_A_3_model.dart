f97f86cef0c84a89e0c0597c00a746a05458d370
Latefah00 <148562868+Latefah00@users.noreply.github.com>
2023-10-26 03:44:55 +0300

    Update Q_V_A_A_3_model.dart
    add the code

--- lib/pages/Q_V_A_A_3/Q_V_A_A_3_model.dart
+++ lib/pages/Q_V_A_A_3/Q_V_A_A_3_model.dart
@@ -1,1 +1,28 @@
 
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'q_v_a_a3_widget.dart' show QVAA3Widget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class QVAA3Model extends FlutterFlowModel<QVAA3Widget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
