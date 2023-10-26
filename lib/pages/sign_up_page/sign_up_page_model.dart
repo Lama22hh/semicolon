715b89ff7cd4ae77157ef7a305f4dd8784e73ce5
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2023-10-26 09:47:15 +0300

    ‏Make the form clarify the errors used by entering it before pressing the Sign Up button and adding explanatory error messages to each text field.

--- lib/pages/sign_up_page/sign_up_page_model.dart
+++ lib/pages/sign_up_page/sign_up_page_model.dart
@@ -17,7 +17,6 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
   final unfocusNode = FocusNode();
   final formKey = GlobalKey<FormState>();
   // State field(s) for FullNameTextField widget.
-  FocusNode? fullNameTextFieldFocusNode;
   TextEditingController? fullNameTextFieldController;
   String? Function(BuildContext, String?)? fullNameTextFieldControllerValidator;
   String? _fullNameTextFieldControllerValidator(
@@ -32,7 +31,6 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
   }
 
   // State field(s) for UserNameTextField widget.
-  FocusNode? userNameTextFieldFocusNode;
   TextEditingController? userNameTextFieldController;
   String? Function(BuildContext, String?)? userNameTextFieldControllerValidator;
   String? _userNameTextFieldControllerValidator(
@@ -44,13 +42,14 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
     }
 
     if (!RegExp(kTextValidatorUsernameRegex).hasMatch(val)) {
-      return 'Must start with a letter and can only contain letters, digits and - or _.';
+      return FFLocalizations.of(context).getText(
+        '0lemgwvr' /* يجب ان يحتوى على حروف انجليزيه... */,
+      );
     }
     return null;
   }
 
   // State field(s) for PhoneNumberTextField widget.
-  FocusNode? phoneNumberTextFieldFocusNode;
   TextEditingController? phoneNumberTextFieldController;
   String? Function(BuildContext, String?)?
       phoneNumberTextFieldControllerValidator;
@@ -63,13 +62,14 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
     }
 
     if (!RegExp('^\\d{10}\$').hasMatch(val)) {
-      return 'Invalid text';
+      return FFLocalizations.of(context).getText(
+        '5uo87fje' /* يحتوى على عشر ارقام فقط */,
+      );
     }
     return null;
   }
 
   // State field(s) for EmailTextField widget.
-  FocusNode? emailTextFieldFocusNode;
   TextEditingController? emailTextFieldController;
   String? Function(BuildContext, String?)? emailTextFieldControllerValidator;
   String? _emailTextFieldControllerValidator(
@@ -81,13 +81,14 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
     }
 
     if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
-      return 'Has to be a valid email address.';
+      return FFLocalizations.of(context).getText(
+        'jh7a9gm4' /* ادخل الايميل بشكل صحيح  */,
+      );
     }
     return null;
   }
 
   // State field(s) for PasswordTextField widget.
-  FocusNode? passwordTextFieldFocusNode;
   TextEditingController? passwordTextFieldController;
   late bool passwordTextFieldVisibility;
   String? Function(BuildContext, String?)? passwordTextFieldControllerValidator;
@@ -99,15 +100,22 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
       );
     }
 
+    if (val.length < 8) {
+      return FFLocalizations.of(context).getText(
+        'zvdsmtix' /* اقل طول هو ٨ */,
+      );
+    }
+
     if (!RegExp('^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?!.*\\W).+\$')
         .hasMatch(val)) {
-      return 'Invalid text';
+      return FFLocalizations.of(context).getText(
+        '1p2418q9' /* يحتوى على على الاقل على رقم و ... */,
+      );
     }
     return null;
   }
 
   // State field(s) for RePasswordTextField widget.
-  FocusNode? rePasswordTextFieldFocusNode;
   TextEditingController? rePasswordTextFieldController;
   late bool rePasswordTextFieldVisibility;
   String? Function(BuildContext, String?)?
@@ -117,6 +125,12 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
     if (val == null || val.isEmpty) {
       return FFLocalizations.of(context).getText(
         'ovm3bh2x' /* ادخل المطلوب */,
+      );
+    }
+
+    if (val.length < 8) {
+      return FFLocalizations.of(context).getText(
+        'aiga045e' /* اقل طول هو ٨ */,
       );
     }
 
@@ -149,22 +163,11 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
 
   void dispose() {
     unfocusNode.dispose();
-    fullNameTextFieldFocusNode?.dispose();
     fullNameTextFieldController?.dispose();
-
-    userNameTextFieldFocusNode?.dispose();
     userNameTextFieldController?.dispose();
-
-    phoneNumberTextFieldFocusNode?.dispose();
     phoneNumberTextFieldController?.dispose();
-
-    emailTextFieldFocusNode?.dispose();
     emailTextFieldController?.dispose();
-
-    passwordTextFieldFocusNode?.dispose();
     passwordTextFieldController?.dispose();
-
-    rePasswordTextFieldFocusNode?.dispose();
     rePasswordTextFieldController?.dispose();
   }
 
@@ -172,3 +175,4 @@ class SignUpPageModel extends FlutterFlowModel<SignUpPageWidget> {
 
   /// Additional helper methods are added here.
 }
+
