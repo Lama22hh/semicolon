7de98e87eb66d21314c93b7a6e3f7720e3f3333b
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2023-12-26 13:13:39 +0300

    add quize widget model component

---
+++ lib/components/quize_widget_model.dart
@@ -0,0 +1,34 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_radio_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'quize_widget_widget.dart' show QuizeWidgetWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class QuizeWidgetModel extends FlutterFlowModel<QuizeWidgetWidget> {
+  ///  Local state fields for this component.
+
+  String answer = 'null';
+
+  ///  State fields for stateful widgets in this component.
+
+  // State field(s) for RadioButton widget.
+  FormFieldController<String>? radioButtonValueController;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+
+  String? get radioButtonValue => radioButtonValueController?.value;
+}
