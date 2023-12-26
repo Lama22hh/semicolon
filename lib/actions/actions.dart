a36ca7559d60b17de5caac6bd15e637ff905efd9
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2023-12-26 13:59:34 +0300

    Add action file

--- lib/actions/actions.dart
+++ lib/actions/actions.dart
@@ -1,1 +1,32 @@
-.
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+
+Future tryit(
+  BuildContext context, {
+  String? link,
+}) async {
+  ContentRecord? content;
+
+  content = await queryContentRecordOnce(
+    queryBuilder: (contentRecord) => contentRecord
+        .where(
+          'nameOfCourse',
+          isEqualTo: 'java',
+        )
+        .where(
+          'typeOfContent',
+          isEqualTo: 'article ',
+        )
+        .where(
+          'nameOfLesson',
+          isEqualTo: 'java data types ',
+        ),
+    singleRecord: true,
+  ).then((s) => s.firstOrNull);
+}
