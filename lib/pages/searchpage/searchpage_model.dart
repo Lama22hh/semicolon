bcd004339e8c3fd0edb8c6fe6ce5c01dedf2a0d8
khawla5 <60198041+khawla5@users.noreply.github.com>
2023-12-26 21:12:14 +0300

    Create searchpage_model.dart

---
+++ lib/pages/searchpage/searchpage_model.dart
@@ -0,0 +1,46 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'searchpage_widget.dart' show SearchpageWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:text_search/text_search.dart';
+
+class SearchpageModel extends FlutterFlowModel<SearchpageWidget> {
+  ///  Local state fields for this page.
+
+  bool serachactive = false;
+
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  List<ContentRecord> simpleSearchResults = [];
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode;
+  TextEditingController? textController;
+  String? Function(BuildContext, String?)? textControllerValidator;
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+    textFieldFocusNode?.dispose();
+    textController?.dispose();
+
+    tabBarController?.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
