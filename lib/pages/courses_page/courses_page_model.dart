10366116630e80cdbe24532512bd7470dbf4db53
Latefah00 <148562868+Latefah00@users.noreply.github.com>
2023-12-26 20:57:41 +0300

    Update courses_page_model.dart
    adding the courses_page model

--- lib/pages/courses_page/courses_page_model.dart
+++ lib/pages/courses_page/courses_page_model.dart
@@ -1,1 +1,45 @@
+import '/backend/backend.dart';
+import '/components/quize_widget_widget.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_web_view.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/flutter_flow_youtube_player.dart';
+import 'content_page_widget.dart' show ContentPageWidget;
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ContentPageModel extends FlutterFlowModel<ContentPageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode;
+  TextEditingController? textController;
+  String? Function(BuildContext, String?)? textControllerValidator;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+    tabBarController?.dispose();
+    textFieldFocusNode?.dispose();
+    textController?.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
 
