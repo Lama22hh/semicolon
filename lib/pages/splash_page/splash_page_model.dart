d37a48806468dcca4f8bac53745f9c8e3e4be543
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2023-12-26 16:24:45 +0300

    Update splash_page_model.dart

--- lib/pages/splash_page/splash_page_model.dart
+++ lib/pages/splash_page/splash_page_model.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
 
 class SplashPageModel extends FlutterFlowModel<SplashPageWidget> {
   /// Initialization and disposal methods.
