9baa211701c01ef22a47b46e7ebd8eec1c6ddd8d
Lama22hh <148897638+Lama22hh@users.noreply.github.com>
2023-12-26 14:15:39 +0300

    Update posts_record.dart

--- lib/backend/schema/posts_record.dart
+++ lib/backend/schema/posts_record.dart
@@ -1,1 +1,113 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+
+import '/backend/schema/util/firestore_util.dart';
+import '/backend/schema/util/schema_util.dart';
+
+import 'index.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+
+class PostsRecord extends FirestoreRecord {
+  PostsRecord._(
+    DocumentReference reference,
+    Map<String, dynamic> data,
+  ) : super(reference, data) {
+    _initializeFields();
+  }
+
+  // "aurthor" field.
+  DocumentReference? _aurthor;
+  DocumentReference? get aurthor => _aurthor;
+  bool hasAurthor() => _aurthor != null;
+
+  // "text" field.
+  String? _text;
+  String get text => _text ?? '';
+  bool hasText() => _text != null;
+
+  // "timestamp" field.
+  DateTime? _timestamp;
+  DateTime? get timestamp => _timestamp;
+  bool hasTimestamp() => _timestamp != null;
+
+  DocumentReference get parentReference => reference.parent.parent!;
+
+  void _initializeFields() {
+    _aurthor = snapshotData['aurthor'] as DocumentReference?;
+    _text = snapshotData['text'] as String?;
+    _timestamp = snapshotData['timestamp'] as DateTime?;
+  }
+
+  static Query<Map<String, dynamic>> collection([DocumentReference? parent]) =>
+      parent != null
+          ? parent.collection('posts')
+          : FirebaseFirestore.instance.collectionGroup('posts');
+
+  static DocumentReference createDoc(DocumentReference parent) =>
+      parent.collection('posts').doc();
+
+  static Stream<PostsRecord> getDocument(DocumentReference ref) =>
+      ref.snapshots().map((s) => PostsRecord.fromSnapshot(s));
+
+  static Future<PostsRecord> getDocumentOnce(DocumentReference ref) =>
+      ref.get().then((s) => PostsRecord.fromSnapshot(s));
+
+  static PostsRecord fromSnapshot(DocumentSnapshot snapshot) => PostsRecord._(
+        snapshot.reference,
+        mapFromFirestore(snapshot.data() as Map<String, dynamic>),
+      );
+
+  static PostsRecord getDocumentFromData(
+    Map<String, dynamic> data,
+    DocumentReference reference,
+  ) =>
+      PostsRecord._(reference, mapFromFirestore(data));
+
+  @override
+  String toString() =>
+      'PostsRecord(reference: ${reference.path}, data: $snapshotData)';
+
+  @override
+  int get hashCode => reference.path.hashCode;
+
+  @override
+  bool operator ==(other) =>
+      other is PostsRecord &&
+      reference.path.hashCode == other.reference.path.hashCode;
+}
+
+Map<String, dynamic> createPostsRecordData({
+  DocumentReference? aurthor,
+  String? text,
+  DateTime? timestamp,
+}) {
+  final firestoreData = mapToFirestore(
+    <String, dynamic>{
+      'aurthor': aurthor,
+      'text': text,
+      'timestamp': timestamp,
+    }.withoutNulls,
+  );
+
+  return firestoreData;
+}
+
+class PostsRecordDocumentEquality implements Equality<PostsRecord> {
+  const PostsRecordDocumentEquality();
+
+  @override
+  bool equals(PostsRecord? e1, PostsRecord? e2) {
+    return e1?.aurthor == e2?.aurthor &&
+        e1?.text == e2?.text &&
+        e1?.timestamp == e2?.timestamp;
+  }
+
+  @override
+  int hash(PostsRecord? e) =>
+      const ListEquality().hash([e?.aurthor, e?.text, e?.timestamp]);
+
+  @override
+  bool isValidKey(Object? o) => o is PostsRecord;
+}
 
