df0c1c86d7a1603dca0f0bb346e52c2c0ea666b6
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2023-12-26 13:32:46 +0300

    Add files via upload

---
+++ lib/index.dart
@@ -0,0 +1,45 @@
+// Export pages
+export '/pages/splash_page/splash_page_widget.dart' show SplashPageWidget;
+export '/pages/choose_lang_page/choose_lang_page_widget.dart'
+    show ChooseLangPageWidget;
+export '/pages/frist_page/frist_page_widget.dart' show FristPageWidget;
+export '/pages/setting_page/setting_page_widget.dart' show SettingPageWidget;
+export '/change_password_arabic_page/change_password_arabic_page_widget.dart'
+    show ChangePasswordArabicPageWidget;
+export '/change_password_english_page/change_password_english_page_widget.dart'
+    show ChangePasswordEnglishPageWidget;
+export '/pages/assignments_interface_english/assignments_interface_english_widget.dart'
+    show AssignmentsInterfaceEnglishWidget;
+export '/pages/assignments_interface_arabic/assignments_interface_arabic_widget.dart'
+    show AssignmentsInterfaceArabicWidget;
+export '/pages/sign_in_page/sign_in_page_widget.dart' show SignInPageWidget;
+export '/pages/sign_up_page/sign_up_page_widget.dart' show SignUpPageWidget;
+export '/change_password/change_password_widget.dart' show ChangePasswordWidget;
+export '/pages/forgot_password/forgot_password_widget.dart'
+    show ForgotPasswordWidget;
+export '/pages/ass_page/ass_page_widget.dart' show AssPageWidget;
+export '/change_password_page/change_password_page_widget.dart'
+    show ChangePasswordPageWidget;
+export '/introductiontojavaquizes/introductiontojavaquizes_widget.dart'
+    show IntroductiontojavaquizesWidget;
+export '/pages/data_typequizes/data_typequizes_widget.dart'
+    show DataTypequizesWidget;
+export '/stringquizes/stringquizes_widget.dart' show StringquizesWidget;
+export '/pages/arrayquizes/arrayquizes_widget.dart' show ArrayquizesWidget;
+export '/pages/if_else_statmentquizes/if_else_statmentquizes_widget.dart'
+    show IfElseStatmentquizesWidget;
+export '/information_page/information_page_widget.dart'
+    show InformationPageWidget;
+export '/pages/content_page/content_page_widget.dart' show ContentPageWidget;
+export '/pages/courses_page/courses_page_widget.dart' show CoursesPageWidget;
+export '/pages/lesson_page/lesson_page_widget.dart' show LessonPageWidget;
+export '/pages/delete_account/delete_account_widget.dart'
+    show DeleteAccountWidget;
+export '/pages/change_password_page_secsses/change_password_page_secsses_widget.dart'
+    show ChangePasswordPageSecssesWidget;
+export '/pages/topics/topics_widget.dart' show TopicsWidget;
+export '/posts/posts_widget.dart' show PostsWidget;
+export '/pages/add_topic/add_topic_widget.dart' show AddTopicWidget;
+export '/searchpage_topic/searchpage_topic_widget.dart'
+    show SearchpageTopicWidget;
+export '/searchpage/searchpage_widget.dart' show SearchpageWidget;
