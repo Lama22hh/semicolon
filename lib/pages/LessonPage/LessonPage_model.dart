d577a74e5556b70ffe23559eb048ddb3a64fd3e2
Lama22hh <148897638+Lama22hh@users.noreply.github.com>
2024-01-14 21:51:20 +0300

    Update LessonPage_model.dart

--- lib/pages/LessonPage/LessonPage_model.dart
+++ lib/pages/LessonPage/LessonPage_model.dart
@@ -1,9 +1,12 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'lesson_page_widget.dart' show LessonPageWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
