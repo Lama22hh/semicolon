4b53ec285f2b34f0abb8dfb7d7539c29306ab9ec
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2024-01-14 20:29:50 +0300

    Update content_page_model.dart

--- lib/pages/content_page/content_page_model.dart
+++ lib/pages/content_page/content_page_model.dart
@@ -1,3 +1,4 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/components/quize_widget_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
@@ -8,6 +9,7 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/flutter_flow_youtube_player.dart';
 import 'content_page_widget.dart' show ContentPageWidget;
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -42,4 +44,3 @@ class ContentPageModel extends FlutterFlowModel<ContentPageWidget> {
 
   /// Additional helper methods are added here.
 }
-
