188cd89aa0bf4e33b9c1fb144bcef5b444e60ad6
Latefah00 <148562868+Latefah00@users.noreply.github.com>
2024-01-16 22:15:26 +0300

    Update CompetitionBoard_model.dart
    add code

--- lib/pages/CompetitionBoard/CompetitionBoard_model.dart
+++ lib/pages/CompetitionBoard/CompetitionBoard_model.dart
@@ -1,1 +1,29 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'competition_board_widget.dart' show CompetitionBoardWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class CompetitionBoardModel extends FlutterFlowModel<CompetitionBoardWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    unfocusNode.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
 
